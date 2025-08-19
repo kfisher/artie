5ccb8b76344479aef9a1248ac1dcfcbf77f986b2
Kevin Fisher <kfisher@users.noreply.github.com>
2025-08-18 21:10:47 -0400

    Restructure optical_drive crate.

--- optical_drive/src/linux.rs
+++ optical_drive/src/linux.rs
@@ -1,8 +1,6 @@
 // Copyright 2025 Kevin Fisher. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 
-//! # Optical Drive - Linux
-//!
 //! Linux implementation for OS specific code for interfacing with the optical drives.
 
 use std::process::Command;
