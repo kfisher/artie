9e9bd7b8bbe6a0675a5b4c88e7560001d1b60c36
Kevin Fisher <kfisher@users.noreply.github.com>
2025-09-05 20:44:57 -0400

    Update workspace configuration.
    Changed the workspace from a virtual workspace to one with a root
    package for the main executable. Updated the member workspaces to used
    some of the shared workspace settings.

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// Copyright 2025 Kevin Fisher. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-only
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("Hello, World!");
+    Ok(())
+}
