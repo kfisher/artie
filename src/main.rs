7fbb008d36f391e7455fbd7c9f041f794b77ace4
Kevin Fisher <kfisher@users.noreply.github.com>
2025-09-06 07:39:47 -0400

    Add iced and hello-world GUI.
    Added `iced` as a dependency using git instead of cargo to get the
    latest features of the framework. Created a basic hello world type
    application window.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
 // Copyright 2025 Kevin Fisher. All rights reserved.
 // SPDX-License-Identifier: GPL-3.0-only
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Hello, World!");
-    Ok(())
+use iced::Element;
+use iced::widget::text;
+
+fn main() -> iced::Result {
+    iced::application(Artie::default, Artie::update, Artie::view)
+        .title("Artie")
+        .run()
 }
+
+/// Specifies the application messages.
+///
+/// Application messages are essentially the interactions of the application. Whenever the user 
+/// interacts with the application, the interaction will trigger an application update. See
+/// [`Artie::update`] for more information. Note that interactions are not necessarily limited to
+/// user interactions.
+#[derive(Clone, Debug)]
+enum Message {
+}
+
+/// TODO: DOC
+#[derive(Default)]
+struct Artie {
+}
+
+impl Artie {
+    /// Processes interactions to update the state of the application.
+    fn update(&mut self, message: Message) {
+        match message {
+        }
+    }
+
+    /// Uses the current application state to generate the view.
+    fn view(&self) -> Element<'_, Message> {
+        text("Hello, Friend!").into()
+    }
+}
+
+
