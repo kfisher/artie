6463817802ade95cad39e51aa49f4b4f49b0aab8
Kevin Fisher <kfisher@users.noreply.github.com>
2025-10-11 09:06:12 -0400

    Add fs crate.

---
+++ fs/src/lib.rs
@@ -0,0 +1,8 @@
+// Copyright 2025 Kevin Fisher. All rights reserved.
+// SPDX-License-Identifier: GPL-3.0-only
+
+//! Crate responsible for file system operations.
+//!
+//! This crate is used to perform file system operations related to managing media and data files
+//! that are generated when performing copy and transcode operations. 
+
