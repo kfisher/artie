2d90162b6e25c36b4a2b23c16226051ee33778a2
Kevin Fisher <kfisher@users.noreply.github.com>
2025-12-22 10:51:44 -0500

    Import drive changes.
    Imported changes to the optical drive module from another branch. Not
    all changes were imported, so some of what was is commented out.

--- src/drive/faux.rs
+++ src/drive/faux.rs
@@ -3,20 +3,22 @@
 
 //! Faux implementation for development and testing purposes.
 
+//! Faux implementation for development and testing purposes.
+
 use std::sync::LazyLock;
 
 use crate::Result;
 
-use super::{DiscState, OpticalDrive};
+use super::{DiscState, OsOpticalDrive};
 
-static FAUX_DRIVES: LazyLock<Vec<OpticalDrive>> = LazyLock::new(|| {
+static FAUX_DRIVES: LazyLock<Vec<OsOpticalDrive>> = LazyLock::new(|| {
     vec![
-        OpticalDrive {
+        OsOpticalDrive {
             path: String::from("/dev/fx0"),
             serial_number: String::from("FAUX0001"),
             disc: DiscState::None,
         },
-        OpticalDrive {
+        OsOpticalDrive {
             path: String::from("/dev/fx1"),
             serial_number: String::from("FAUX0002"),
             disc: DiscState::Inserted {
@@ -27,6 +29,16 @@ static FAUX_DRIVES: LazyLock<Vec<OpticalDrive>> = LazyLock::new(|| {
     ]
 });
 
+/// Gets the optical drive information for all available optical drives.
+///
+/// This is a fake implementation only meant for development and testing where the development
+/// system may not have optical drives or when it might not be desireable to use actual drives
+/// such as automated tests.
+pub fn get_optical_drives() -> Result<Vec<OsOpticalDrive>> {
+    let drives = &*FAUX_DRIVES;
+    Ok(drives.clone())
+}
+
 /// Gets the optical drive information for an optical drive with serial number
 /// `serial_number`.
 ///
@@ -34,8 +46,10 @@ static FAUX_DRIVES: LazyLock<Vec<OpticalDrive>> = LazyLock::new(|| {
 /// number. Returns an error if something goes wrong when querying the operating
 /// system.
 ///
-/// This is the Linux specific implementation.
-pub fn get_optical_drive(serial_number: &str) -> Result<Option<OpticalDrive>> {
+/// This is a fake implementation only meant for development and testing where the development
+/// system may not have optical drives or when it might not be desireable to use actual drives
+/// such as automated tests.
+pub fn get_optical_drive(serial_number: &str) -> Result<Option<OsOpticalDrive>> {
     for drive in &*FAUX_DRIVES {
         if drive.serial_number == serial_number {
             return Ok(Some(drive.clone()));
--- src/drive/mod.rs
+++ src/drive/mod.rs
@@ -11,25 +11,22 @@ mod faux;
 
 /// Platform specific code.
 mod platform {
-    #[cfg(target_os = "linux")]
+    #[cfg(all(target_os = "linux", not(feature = "faux_drives")))]
+    pub use super::linux::get_optical_drives;
+
+    #[cfg(all(target_os = "linux", not(feature = "faux_drives")))]
     pub use super::linux::get_optical_drive;
+
+    #[cfg(feature = "faux_drives")]
+    pub use super::faux::get_optical_drives;
+
+    #[cfg(feature = "faux_drives")]
+    pub use super::faux::get_optical_drive;
 }
 
-/// Represents an optical drive.
-#[derive(Clone, Debug, PartialEq)]
-pub struct OpticalDrive {
-    /// The device path of the drive, such as "/dev/sr0".
-    pub path: String,
+use std::time::Duration;
 
-    /// The serial number of the optical drive.
-    ///
-    /// This may be a shortened version of the serial number assigned by the
-    /// manufacturer.
-    pub serial_number: String,
-
-    /// The state of the disc in the optical drive.
-    pub disc: DiscState,
-}
+use crate::Result;
 
 /// Represents the state of the optical drive's disc.
 #[derive(Clone, Debug, PartialEq)]
@@ -44,19 +41,138 @@ pub enum DiscState {
     Inserted { label: String, uuid: String },
 }
 
-//--] /// Gets the optical drive information for an optical drive with serial number
-//--] /// `serial_number`.
-//--] ///
-//--] /// Returns `None` if an optical drive cannot be found with the provided serial
-//--] /// number. Returns an error if something goes wrong when querying the operating
-//--] /// system.
-//--] pub fn get_optical_drive(serial_number: &str) -> Result<Option<OpticalDrive>> {
-//--]     let drive = platform::get_optical_drive(serial_number)?;
-//--] 
-//--]     #[cfg(feature = "faux_drives")]
-//--]     if drive.is_none() {
-//--]         return faux::get_optical_drive(serial_number);
-//--]     }
-//--] 
-//--]     Ok(drive)
-//--] }
+/// Represents the state of the optical drive.
+#[derive(Clone, Debug, PartialEq)]
+pub enum OpticalDriveState {
+    /// The drive information is no longer available.
+    ///
+    /// The main reasons a user may see this state is if they disconnect the drive after the
+    /// application starts (e.g. unplug the USB device) or the worker node handling the drive
+    /// stops communicating.
+    Disconnected,
+
+    /// The drive is waiting to begin a copy operation.
+    Idle,
+
+    /// The drive is in the process of copying a disc.
+    Copying {
+        /// The current stage of the copying process.
+        stage: String,
+
+        /// The task currently being performed.
+        task: String,
+
+        /// The percent complete (0 -> 0%, 1.0 -> 100%) of the current task.
+        task_progress: f32,
+
+        /// The subtask currently being performed.
+        subtask: String,
+
+        /// The percent complete (0 -> 0%, 1.0 -> 100%) of the current subtask.
+        subtask_progress: f32,
+
+        /// The length of time the copy operation has been running.
+        elapsed_time: Duration,
+    },
+
+    /// The copy operation completed successfully.
+    ///
+    /// Once the copy operation completes, the drive will remain in this state until acknowledged
+    /// which will reset the state back to `Idle`.
+    Success,
+
+    /// The copy operation failed or was cancelled.
+    ///
+    /// The drive will remain in this state until acknkowledged which will reset the state back to
+    /// `Idle`.
+    Failed {
+        /// Brief description of what caused the failure.
+        error: String,
+    },
+}
+
+/// Represents an optical drive.
+#[derive(Clone, Debug)]
+pub struct OpticalDrive {
+    /// The name assigned to the drive.
+    ///
+    /// This will be set to the serial number by default, but can be overwritten by the user.
+    pub name: String,
+
+    /// The device path of the drive, such as "/dev/sr0".
+    pub path: String,
+
+    /// The serial number of the optical drive.
+    ///
+    /// This may be a shortened version of the serial number assigned by the
+    /// manufacturer.
+    pub serial_number: String,
+
+    /// The state of the disc in the optical drive.
+    pub disc: DiscState,
+
+    /// The state of the drive.
+    pub state: OpticalDriveState,
+
+    //-]/// Interface for communicating with the actor responsible for this drive instance.
+    //-]pub handle: DriveActorHandle,
+}
+
+impl OpticalDrive {
+    /// Create a [`OpticalDrive`] instance from OS provided optical drive information.
+    fn from_os(value: OsOpticalDrive) -> Self {
+        Self {
+            //-]handle: actor::create_actor(&value.serial_number),
+            name: value.serial_number.clone(),
+            path: value.path,
+            serial_number: value.serial_number,
+            disc: value.disc,
+            state: OpticalDriveState::Idle,
+        }
+    }
+}
+
+/// Initialize the optical drive information for all available drives reported by the OS.
+///
+/// # Errors
+///
+/// - [`crate::Error::CommandIo`] or [`crate::Error::CommandReturnedErrorCode`] if the command to
+///   to get the optical drive from the OS fails, or
+/// - [`crate::Error::Serialization`] if the output from the OS cannot be parsed
+pub fn init() -> Result<Vec<OpticalDrive>> {
+    let drives = get_optical_drives()?.into_iter()
+        .map(|d| OpticalDrive::from_os(d))
+        .collect();
+    Ok(drives)
+}
+
+/// Represents an optical drive.
+#[derive(Clone, Debug, PartialEq)]
+struct OsOpticalDrive {
+    /// The device path of the drive, such as "/dev/sr0".
+    pub path: String,
+
+    /// The serial number of the optical drive.
+    ///
+    /// This may be a shortened version of the serial number assigned by the
+    /// manufacturer.
+    pub serial_number: String,
+
+    /// The state of the disc in the optical drive.
+    pub disc: DiscState,
+}
+
+/// Gets the optical drive information for all available optical drives.
+fn get_optical_drives() -> Result<Vec<OsOpticalDrive>> {
+  let drives = platform::get_optical_drives()?;
+  Ok(drives)
+}
+
+/// Gets the optical drive information for an optical drive with serial number `serial_number`.
+///
+/// Returns `None` if an optical drive cannot be found with the provided serial number. Returns an
+/// error if something goes wrong when querying the operating system.
+fn get_optical_drive(serial_number: &str) -> Result<Option<OsOpticalDrive>> {
+  let drive = platform::get_optical_drive(serial_number)?;
+  Ok(drive)
+}
