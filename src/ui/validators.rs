02ccec828962667a312e636a58b07c6e9c2d97b2
Kevin Fisher <kfisher@users.noreply.github.com>
2026-07-05 08:50:37 -0400

    Add missed file.
    This should have been committed when the dropdown widget was added
    several commits ago.

--- src/ui/validators.rs
+++ src/ui/validators.rs
@@ -1,7 +1,22 @@
 // Copyright 2026 Kevin Fisher. All rights reserved.
 // SPDX-License-Identifier: GPL-3.0-only
 
-//! UI common validation functions.
+//! TODO
+
+use std::boxed::Box;
+
+// TODO
+pub trait Validate {
+    // TODO
+    fn validate(&self) -> bool;
+}
+
+// TODO
+#[derive(Default)]
+pub struct Validator {
+    // TODO
+    validators: Vec<Box<dyn Validate>>,
+}
 
 #[cfg(test)]
 mod tests {
